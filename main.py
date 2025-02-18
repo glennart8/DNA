7a1da0b9672a6d8fdffa026cb787dc2196fce6b0
glennart8 <henrikpilback@gmail.com>
2025-02-18 10:26:04 +0100

    En bit på väg. Påbörjat en metod som separerar keys från values. ska jobba på hur den räknar ihop values.

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
-# regular expression
 import re
-text_path = "dna_raw.txt" # relative path
+
+# Läs in fil
+text_path = "dna_raw.txt"
 with open(text_path, "r") as file:
     raw_text = file.read()
 
-# Separera sekvenserna 
-# Gör alla bokstäver små
-# Skapa dictionarys av varje sekvens där seq1 är nyckeln och cGTA... är value
-# Räkna antalet av resp bokstav
+
+def count_dna(raw_text):
+    dna_dictionary = {}
+    sequences = raw_text.strip().split('>') # 
+
+    for sequence in sequences[1:]: # hoppa över det första tomma
+        lines = sequence.strip().split("'\n") # delat alla tecken till egna rader och vid ny rad
+        key = lines[0]
+        value = ''.join(lines[1:]).lower()
+        
+        dna_counting = {
+            'a': value.count('a'),
+            't': value.count('t'),
+            'c': value.count('c'),
+            'g': value.count('g'),
+        }
+
+        dna_dictionary[key] = dna_counting
+
+    return dna_dictionary
+        # därefter, spara resterande bokstäver till value
+
+
+    # dela sekvenserna i par 
+    
+    # gör en dictionary av varje sekvens och räkna antalet bokstäver
+result = count_dna(raw_text)
+
+for key, value in result.items():
+    print(f"Sequence: {key}")
+    print(f"DNA counts: {value}")
+    print()
+
+
+
+
 # Visa diagram över frekvensen av bokstäver
-
-
-print(raw_text)
-
-
-
-text_fixed_spacing = re.sub(r"\s+"," ", raw_text)
-text_list = [text.capitalize() for text in text_fixed_spacing.split(". ")]
-
-# join with newlines for each sentence, we could choose another design
-cleaned_text = ".\n".join(text_list)
-
-
